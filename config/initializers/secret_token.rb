4f0bc5483a02fc5dd81b15c82555b7d5195ae5f3
Shane Cunningham <Cunningham.shane@hotmail.com>
2011-12-20 16:53:06 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Camera::Application.config.secret_token = 'eec7137c9f0dc8a009c5c1ff4cf04f756b92333406be343454292811ec3cc8b4959f2950b96a5593ddba101533c75542d2597d2c82b1d039214fcb10296022d8'
